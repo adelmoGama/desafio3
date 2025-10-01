414e30180dc04fe8bfce427848856026b0f484b1
Adelmo Gama <gama.a.jose@gmail.com>
2025-10-01 10:43:28 -0300

    Paginação no findAll finalizada

--- src/main/java/com/devsuperior/desafio3/controllers/ClientController.java
+++ src/main/java/com/devsuperior/desafio3/controllers/ClientController.java
@@ -3,6 +3,8 @@ package com.devsuperior.desafio3.controllers;
 import com.devsuperior.desafio3.dto.ClientDTO;
 import com.devsuperior.desafio3.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,4 +23,10 @@ public class ClientController {
         ClientDTO dto = clientService.findById(id);
         return ResponseEntity.ok(dto);
     }
+    @GetMapping
+    public ResponseEntity<Page<ClientDTO>> findAll(Pageable pageable) {
+        Page<ClientDTO> results = clientService.findAll(pageable);
+        return ResponseEntity.ok(results);
+    }
+
 }
