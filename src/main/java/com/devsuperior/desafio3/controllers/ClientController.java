eca9ad90cd28a151222e6d8cb5d220469a5ef440
Adelmo Gama <gama.a.jose@gmail.com>
2025-10-01 10:36:21 -0300

    Cramada do controlador funcional

---
+++ src/main/java/com/devsuperior/desafio3/controllers/ClientController.java
@@ -0,0 +1,24 @@
+package com.devsuperior.desafio3.controllers;
+
+import com.devsuperior.desafio3.dto.ClientDTO;
+import com.devsuperior.desafio3.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/clients")
+public class ClientController {
+
+    @Autowired
+    private ClientService clientService;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id) {
+        ClientDTO dto = clientService.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+}
