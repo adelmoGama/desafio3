64aa43cffcabaf161a59d8ab97e5834bd1110d8f
Adelmo Gama <gama.a.jose@gmail.com>
2025-10-01 14:10:30 -0300

    VaProjeto finalizado

--- src/main/java/com/devsuperior/desafio3/controllers/handler/ControllerExceptionHandler.java
+++ src/main/java/com/devsuperior/desafio3/controllers/handler/ControllerExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.devsuperior.desafio3.controllers.handler;
 
 import com.devsuperior.desafio3.dto.CustomError;
+import com.devsuperior.desafio3.dto.ValidationError;
 import com.devsuperior.desafio3.services.exceptions.DatabaseException;
 import com.devsuperior.desafio3.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -14,10 +17,13 @@ import java.time.Instant;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
+    private static final String INVALID_DATA = "Invalid data.";
+    private static final String NOT_FOUND_DATA = "Client not founded.";
+
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
-        CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        CustomError err = new CustomError(Instant.now(), status.value(), NOT_FOUND_DATA, request.getRequestURI());
         return ResponseEntity.status(status).body(err);
     }
 
@@ -25,6 +31,18 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> database(DatabaseException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.BAD_REQUEST;
         CustomError err = new CustomError(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError err = new ValidationError(Instant.now(), status.value(), INVALID_DATA, request.getRequestURI());
+
+        for(FieldError f : e.getBindingResult().getFieldErrors()) {
+            err.addError(f.getField(), f.getDefaultMessage());
+        }
+
         return ResponseEntity.status(status).body(err);
     }
 }
